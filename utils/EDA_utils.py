75ff3cd32f2062211e77150e2c01e1ba7ca855c3
Denys Herasymuk <herasymuk@ucu.edu.ua>
2022-07-07 00:13:11 +0300

    Added nulls_simulator and notebook with EDA for some null scenarios

---
+++ utils/EDA_utils.py
@@ -0,0 +1,55 @@
+import matplotlib
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def set_default_plot_properties():
+    plt.style.use('mpl20')
+    matplotlib.rcParams['figure.dpi'] = 100
+    matplotlib.rcParams['figure.figsize'] = 15, 5
+
+
+def sns_set_size(height, width):
+    sns.set(rc={'figure.figsize':(width, height)})
+
+
+def plot_column_grouped_by_race(data, plot_column):
+    sns_set_size(height=18, width=15)
+
+    fig = plt.figure()
+    gs0 = matplotlib.gridspec.GridSpec(ncols=3, nrows=3, figure=fig)
+    race_names = ['White alone', 'Black or African American alone', 'American Indian alone',
+                  'Alaska Native alone', 'American Indian', 'Asian alone',
+                  'Hawaiian and Other Pacific Islander alone', 'Some Other Race alone', 'Two or More Races']
+
+    for i in range(len(race_names)):
+        n_row, n_col = divmod(i, 3)
+        ax = fig.add_subplot(gs0[n_row, n_col])
+        ax.set_title(race_names[i])
+        sns.distplot(data[data['RAC1P'] == i + 1][plot_column], ax=ax)
+
+    plt.show()
+    set_default_plot_properties()
+
+
+def null_scenario_analysis(data, corrupted_data, target_col, condition_col, special_values):
+    # Count of nulls
+    print(f'Count of nulls for {target_col} column: {corrupted_data[target_col].isnull().sum()}')
+    print(f'Count of all records in {target_col} column: {data[target_col].count()}')
+    print(f'Count of records in {condition_col} column in the defined condition: {data[data[condition_col].isin(special_values)][target_col].count()}\n\n')
+
+    # Print density plots for the target column before and after the corruption
+    plt.figure()
+    sns.displot(data[target_col]).set(title=f'Density of {target_col} Column Before Corruption')
+    plt.show()
+
+    plt.figure()
+    sns.displot(corrupted_data[target_col]).set(title=f'Density of {target_col} Column After Corruption')
+    plt.show()
+
+    # Print density plots for AGEP split by race before and after the corruption
+    plot_column = 'AGEP'
+    print(f'Plot {plot_column} column Split by Race [Before Corruption]')
+    plot_column_grouped_by_race(data, plot_column)
+    print(f'\n\n\nPlot {plot_column} column Split by Race [After Corruption]')
+    plot_column_grouped_by_race(corrupted_data[~corrupted_data[target_col].isnull()], plot_column)
--- utils/null_handler.py
+++ utils/null_handler.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
+
 def handle_df_nulls(data, how, column_names):
     '''
     Description: Processes the null values in the dataset
@@ -23,11 +24,54 @@ def handle_df_nulls(data, how, column_names):
     
     if how == 'drop-column':
         data.drop(columns=column_names, inplace=True)
-        
+
     if how == 'drop-rows':
         data.dropna(subset=column_names, inplace=True)
         
-    return data   
+    return data
+
+
+def get_sample_rows(data, target_col, fraction):
+    """
+    Description: create a list of random indexes for rows, which will be used to place nulls in the dataset
+    """
+    n_values_to_discard = int(len(data) * min(fraction, 1.0))
+    perc_lower_start = np.random.randint(0, len(data) - n_values_to_discard)
+    perc_idx = range(perc_lower_start, perc_lower_start + n_values_to_discard)
+
+    depends_on_col = np.random.choice(list(set(data.columns) - {target_col}))
+    # Pick a random percentile of values in other column
+    rows = data[depends_on_col].sort_values().iloc[perc_idx].index
+    return rows
+
+
+def nulls_simulator(data, target_col, condition_col, special_values, fraction, nan_value=np.nan):
+    """
+    Description: simulate nulls for the target column in the dataset based on the condition column and its special values.
+
+    Input:
+    :param data: a dataset, in which nulls values should be simulated
+    :param target_col: a column in the dataset, in which nulls should be placed
+    :param condition_col: a column in the dataset based on which null location should be identified
+    :param special_values: special values for the condition column; special_values and condition_col state the condition,
+        where nulls should be placed
+    :param fraction: float in range [0.0, 1.0], fraction of nulls, which should be placed based on the condition
+    :param nan_value: a value, which should be used as null to be placed in the dataset
+
+    Output: a dataset with null values based on the condition and fraction
+    """
+    if target_col not in data.columns:
+        return ValueError(f'Column {target_col} does not exist in the dataset')
+    if condition_col not in data.columns:
+        return ValueError(f'Column {condition_col} does not exist in the dataset')
+    if not (0 <= fraction <= 1):
+        return ValueError(f'Fraction {fraction} is not in range [0, 1]')
+
+    corrupted_data = data[data[condition_col].isin(special_values)].copy(deep=True)
+    rows = get_sample_rows(corrupted_data, target_col, fraction)
+    corrupted_data.loc[rows, [target_col]] = nan_value
+    corrupted_data = pd.concat([corrupted_data, data[~data[condition_col].isin(special_values)]], axis=0)
+    return corrupted_data
 
 
 def decide_special_category(data):
@@ -37,4 +81,4 @@ def decide_special_category(data):
     if 0 not in data:
         return 0
     else:
-        return max(data)+1
+        return max(data) + 1
